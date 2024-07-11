de26030d5c66db795a82668f0e4d91c69eaebcfa
vishnudeviravi <ivishnudr@gmail.com>
2024-07-11 10:53:51 +0530

    ADDED USER ROUTE

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/user', (req, res) => {
+  console.log('hello user');
+});
+
 app.listen(4000, () => {
   console.log('Working');
 });
